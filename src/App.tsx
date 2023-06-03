c3ec3693389731b79f4aede2a2a18743ddf5a861
Peteris Zevalds <54576938+PeterisZevalds@users.noreply.github.com>
2023-06-03 16:28:52 +0300

    removed a lot of useless packages
    removed a lot of useless packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,50 @@
-import './App.css';
+import { Layout } from 'antd';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+import './App.css';
 import { ProtectedComponent } from './components/ProtectedComponent';
 import { ROUTE_CONFIG } from './routes';
 
+const { Header, Content, Footer } = Layout;
+
 export const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          {ROUTE_CONFIG.map(({ url, component }) => {
-            return <Route key={url} path={url} element={<ProtectedComponent component={component} />} />;
-          })}
-        </Routes>
-      </Router>
-    </div>
+    <StyledLayout>
+      <StyledHeader>
+        <h1>Welcome to my App</h1>
+      </StyledHeader>
+      <StyledContent>
+        <Router>
+          <Routes>
+            {ROUTE_CONFIG.map(({ url, component }) => {
+              return <Route key={url} path={url} element={<ProtectedComponent component={component} />} />;
+            })}
+          </Routes>
+        </Router>
+      </StyledContent>
+      <StyledFooter>© 2023 My App. All rights reserved.</StyledFooter>
+    </StyledLayout>
   );
 };
 
 export default App;
+
+const StyledLayout = styled(Layout)`
+  min-height: 100vh;
+`;
+
+const StyledHeader = styled(Header)`
+  background-color: #007bff;
+  color: #ffffff;
+  padding: 0 1rem;
+`;
+
+const StyledContent = styled(Content)`
+  padding: 1rem;
+`;
+
+const StyledFooter = styled(Footer)`
+  background-color: #f2f2f2;
+  padding: 1rem;
+  text-align: center;
+`;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,23 @@
+import styled from 'styled-components';
+
 const Home = () => {
   return (
-    <section>
-      <h1>Home Page</h1>
-    </section>
+    <HomepageContentWrapperStyled>
+      <HomepageContentStyled>Title</HomepageContentStyled>
+      <div>Bilde</div>
+    </HomepageContentWrapperStyled>
   );
 };
 
 export default Home;
+
+const HomepageContentWrapperStyled = styled.div`
+  display: flex;
+  margin: 40px 0 0;
+  width: 100%;
+`;
+
+const HomepageContentStyled = styled.div`
+  background-color: pink;
+  width: 50%;
+`;
